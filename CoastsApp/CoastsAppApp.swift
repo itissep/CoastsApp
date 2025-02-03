0d7d466ac2ff937529f2685c52d04e3289ffa8ae
itissep <someonenewish@yandex.ru>
2025-02-03 23:15:26 +0300

    Initial Commit

---
+++ CoastsApp/CoastsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoastsAppApp.swift
+//  CoastsApp
+//
+//  Created by Уля on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoastsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
